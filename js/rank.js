ec086090cdcd3d7531c3652c964cfcb21c0634f6
yoolalw <larakemilyprofissional@gmail.com>
2026-06-18 10:55:04 -0300

    Tela Ranking basicamente finalizada || Sem contagem de rank

--- js/rank.js
+++ js/rank.js
@@ -1,4 +1,23 @@
-function home(){
+function home() {
     window.location.href = "home.html"
 }
 
+const usuarios = JSON.parse(localStorage.users)
+const rank = document.getElementById('rank')
+const user = document.getElementById('user')
+const pontos = document.getElementById('pontos')
+
+let cont = 0
+
+JSON.parse(localStorage.users).forEach(u => {
+    rank.innerHTML += `
+        <h1 class="bg-[#aed5f1] rounded-full shadow-lg ">${cont}</h1>
+    `
+    user.innerHTML += `
+        <h1 class="bg-[#aed5f1] rounded-full shadow-lg">${u.user}</h1>
+    `
+    
+    pontos.innerHTML += `
+        <h1 class="bg-[#aed5f1] rounded-full shadow-lg">${u.pontos}</h1>
+    `
+})
